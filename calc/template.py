b956d4216e556818be1742918decb4e96d203f7f
imjanghyeok <qkdrhkgn23@naver.com>
2020-06-28 00:47:00 +0900

    issues#7 input="text" -> input="number"

--- calc/template.py
+++ calc/template.py
@@ -2,8 +2,8 @@ html = b"""
 <html>
  <body>
   <form action="">
-    a= <input type="text" name="a">
-    b= <input type="text" name="b">
+    a= <input type="number" name="a">
+    b= <input type="number" name="b">
     <input type="submit"><br><br>
   </form>
   <p>
