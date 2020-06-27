16efde106307d34e72f6425c432e55d056dd4688
imjanghyeok <qkdrhkgn23@naver.com>
2020-06-28 00:08:52 +0900

    wsgi-issues#4

---
+++ calc/template.py
@@ -0,0 +1,15 @@
+html = b"""
+<html>
+ <body>
+  <form action="">
+    a= <input type="text" name="a">
+    b= <input type="text" name="b">
+    <input type="submit"><br><br>
+  </form>
+  <p>
+   sum
+   prod
+  </p>
+ </body>
+</html>
+"""
