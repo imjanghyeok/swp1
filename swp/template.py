9233b6071b6e61925c236e3fd29af3ce8162e153
imjanghyeok <qkdrhkgn23@naver.com>
2020-06-27 23:35:06 +0900

    issue #1 add mpl.use('Agg')

---
+++ swp/template.py
@@ -0,0 +1,13 @@
+html= b"""
+<html>
+ <body>
+  <form action="">
+   y = <input type="number" name="a"> * x^2
+     + <input type="number" name="b"> * x
+     + <input type="number" name="C"><br><br>
+   <input type="submit">
+  </form>
+  <img src="/usr/local/swp1/swp/img/graph.png" alt="Result image here">
+ </body>
+</html>
+"""
